34d8174a43782cc95d8d4117cb4e161c0632c85b
Eugeney <akimoveugeney@gmail.com>
2024-08-23 17:49:28 +0300

    client auth

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,43 +2,45 @@ import type { Metadata } from 'next'
 import { Noto_Sans } from 'next/font/google'
 import { Toaster } from 'sonner'
 
+import { SITE_NAME } from '@/constants/seo.constants'
+
 import './globals.scss'
 import { Providers } from './providers'
-import { SITE_NAME } from '@/constans/seo.constans'
 
 const noto = Noto_Sans({
-	subsets: ['cyrillic', 'latin'],
-	weight: ['300', '400', '500', '600', '700'],
-	display: 'swap',
-	variable: '--font-noto',
-	style: ['normal']
+  subsets: ['cyrillic', 'latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  display: 'swap',
+  variable: '--font-noto',
+  style: ['normal']
 })
 
 export const metadata: Metadata = {
-	title: {
-		default: SITE_NAME,
-		template: `%s | ${SITE_NAME}`
-	},
-	description: 'Good'
+  title: {
+    default: SITE_NAME,
+    template: `%s | ${SITE_NAME}`
+  },
+  description: 'Good'
 }
 
 export default function RootLayout({
-	children
+  children
 }: Readonly<{
-	children: React.ReactNode
+  children: React.ReactNode
 }>) {
-	return (
-		<html lang='en'>
-			<body className={noto.className}>
-				<Providers>
-					<Toaster
-						theme='dark'
-						position='bottom-right'
-						duration={1500}
-					/>
-					{children}
-				</Providers>
-			</body>
-		</html>
-	)
+  return (
+    <html lang='en'>
+      <body className={noto.className}>
+        <Providers>
+          <Toaster
+            theme='dark'
+            position='bottom-right'
+            duration={1500}
+            className=""
+          />
+          {children}
+        </Providers>
+      </body>
+    </html>
+  )
 }
--- client/src/constans/seo.constans.ts
+++
@@ -1,2 +0,0 @@
-export const NO_INDEX_PAE = {robotos: {index: false, follow: false}}
-export const SITE_NAME = 'PCP'
